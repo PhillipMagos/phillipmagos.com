12fee42d3f6cb03157bbb4e1d9440747c5eae5f5
phillip.magos <phillipmagos@gmail.com>
2023-02-09 10:04:52 -0600

    SP Characters

--- app.js
+++ app.js
@@ -58,6 +58,49 @@ app.get('/checkout', (req, res) => {
   res.render('checkout.ejs')
 })
 
+// Beginning of Southpark Characters
+//Bring in our data - How we bring it out object? - Destructure
+const { characters } = require('./fakeData')
+//console.log(characters)
+
+// Use our public folder
+app.use(express.static('public'))
+// Set view engine
+app.set("view engine", "ejs")
+
+// With more than one / in route
+app.use('/character', express.static(__dirname + '/public'))
+
+
+
+// Routes
+app.get('/', (req, res) => {
+    // modify when building req.query to send data
+    // will use this to build our form
+    res.render("home", {data: characters})
+})
+
+// Display all characters
+app.get('/SPCharacters', (req, res) => {
+    res.render("SPCharacters", {data: characters})
+})
+
+// Display a character 
+app.get('/SPCharacter/:id', (req, res) => {
+    // Could say req.params.id - again destructure
+    const { id } = req.params
+    //console.log(characters[id])
+    res.render("SPCharacter", { data: characters[id]})
+})
+
+
+// Display a character using req.query through form
+app.get("/id", (req, res) => {
+    const { id } = req.query
+    res.render("SPCharacter", {data: characters[id]})
+})
+// End of Southpark Characters
+
 //const port = process.env.PORT || 3000
 //app.listen(port, ()=> console.log(`web host on port ${port}`))
 
